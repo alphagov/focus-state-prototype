03f5a719c2e43a56eae4f017628a26c2109f5678
DebsDee <debs.durojaiye@digital.cabinet-office.gov.uk>
2019-06-13 14:12:54 +0100

    Add routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-module.exports = router
+router.get("/", function(req, res) {
+  res.render("index");
+});
+
+router.post("/new-or-renew-answer", function(req, res) {
+  if (req.session.data["is-new"] === "no") {
+    res.redirect("/date-of-birth");
+  } else {
+    res.redirect("/lost-or-stolen");
+  }
+});
+
+router.post("/lost-or-stolen-answer", function(req, res) {
+  if (req.session.data["is-lost"] === "yes") {
+    res.redirect("/cancel-passport");
+  } else {
+    res.redirect("/date-of-birth");
+  }
+});
+
+module.exports = router;
